35509682da347a8f0ca57fa18abdcb945a395647
Benno Rice <benno@jeamland.net>
2019-12-06 10:52:04 +1100

    Clean up.

--- clojure/day5-2/src/day5_2/core.clj
+++ clojure/day5-2/src/day5_2/core.clj
@@ -12,17 +12,17 @@
   (let [op (nth memory pc)]
     [(mod op 100) (split-modes (int (Math/floor (/ op 100))))]))
 
-(defn value_at_addr [memory addr mode]
+(defn value-at-addr [memory addr mode]
   (case mode
     0 (nth memory (nth memory addr))
     1 (nth memory addr)
     "wtf addr"))
 
-(defn op_3 [memory pc modes func]
+(defn op-3 [memory pc modes func]
   (assoc memory
          (nth memory (+ pc 3))
-         (func (value_at_addr memory (+ pc 1) (nth modes 0 0))
-               (value_at_addr memory (+ pc 2) (nth modes 1 0)))))
+         (func (value-at-addr memory (+ pc 1) (nth modes 0 0))
+               (value-at-addr memory (+ pc 2) (nth modes 1 0)))))
 
 (defn read-stdin [memory pc]
   (print "> ")
@@ -31,7 +31,7 @@
     (assoc memory (nth memory (+ pc 1)) (Integer/parseInt input))))
 
 (defn write-stdout [memory pc mode]
-  (println "#" (value_at_addr memory (+ pc 1) mode))
+  (println "#" (value-at-addr memory (+ pc 1) mode))
   memory)
 
 (defn compare-equal [a b]
@@ -40,23 +40,22 @@
 (defn compare-less [a b]
   (if (< a b) 1 0))
 
+(defn conditional-jump [memory pc modes func]
+  (if (func (value-at-addr memory (+ pc 1) (nth modes 0 0)))
+    (value-at-addr memory (+ pc 2) (nth modes 1 0))
+    (+ pc 3)))
+
 (defn execute [memory pc]
   (let [op (decompose-op memory pc) opcode (first op) modes (second op)]
     (case opcode
-      1 (recur (op_3 memory pc modes +) (+ pc 4))
-      2 (recur (op_3 memory pc modes *) (+ pc 4))
+      1 (recur (op-3 memory pc modes +) (+ pc 4))
+      2 (recur (op-3 memory pc modes *) (+ pc 4))
       3 (recur (read-stdin memory pc) (+ pc 2))
       4 (recur (write-stdout memory pc (nth modes 0 0)) (+ pc 2))
-      5 (recur memory
-               (if (not= 0 (value_at_addr memory (+ pc 1) (nth modes 0 0)))
-                 (value_at_addr memory (+ pc 2) (nth modes 1 0))
-                 (+ pc 3)))
-      6 (recur memory
-               (if (zero? (value_at_addr memory (+ pc 1) (nth modes 0 0)))
-                 (value_at_addr memory (+ pc 2) (nth modes 1 0))
-                 (+ pc 3)))
-      7 (recur (op_3 memory pc modes compare-less) (+ pc 4))
-      8 (recur (op_3 memory pc modes compare-equal) (+ pc 4))
+      5 (recur memory (conditional-jump memory pc modes (fn [x] (not= 0 x))))
+      6 (recur memory (conditional-jump memory pc modes zero?))
+      7 (recur (op-3 memory pc modes compare-less) (+ pc 4))
+      8 (recur (op-3 memory pc modes compare-equal) (+ pc 4))
       99 memory
       "wtf opcode")))
 
